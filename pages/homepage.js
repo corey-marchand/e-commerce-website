33a6ea9b34d5d9bccdf17a51ae000765f2d61156
corey-marchand <corey.marchand@me.com>
2020-07-23 06:48:53 -0700

    initial commit

---
+++ pages/homepage.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Home from '.';
+
+const HomePage = () => {
+    <div className='homepage'>
+        <h1>Welcome to my HomePage</h1>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>Item</div>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>Item</div>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>Item</div>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>Item</div>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>Item</div>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+    </div>
+}
+
+export default HomePage;
